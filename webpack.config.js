e8f7052d962c3babfb172b0e89d03c5cf06525d2
Arturo cortijo Purizaca <arts084@gmail.com>
2021-04-26 09:21:19 -0400

    added HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist/'
+        // Publicpath with dist is for when you don't use contenthash
+        // publicPath: 'dist/'
+        publicPath: ''
     },
     mode: 'none',
     module: {
@@ -48,7 +52,12 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'styles.css'
+            filename: 'styles.[contenthash].css'
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Webpack Starter'
+            //Can also include filename, meta, etc
         })
     ]
 };
